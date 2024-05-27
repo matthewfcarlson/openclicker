000f4e94cf184859c99ffda2e972b8b64d6d623b
Matthew Carlson <matthewfcarlson@users.noreply.github.com>
2024-05-26 21:37:15 -0500

    Add windows version of the build

--- presenter/forge.config.ts
+++ presenter/forge.config.ts
@@ -1,8 +1,8 @@
 import type { ForgeConfig } from '@electron-forge/shared-types';
 import { MakerSquirrel } from '@electron-forge/maker-squirrel';
 import { MakerZIP } from '@electron-forge/maker-zip';
-import { MakerDeb } from '@electron-forge/maker-deb';
-import { MakerRpm } from '@electron-forge/maker-rpm';
+// import { MakerDeb } from '@electron-forge/maker-deb';
+// import { MakerRpm } from '@electron-forge/maker-rpm';
 import { VitePlugin } from '@electron-forge/plugin-vite';
 import { FusesPlugin } from '@electron-forge/plugin-fuses';
 import { FuseV1Options, FuseVersion } from '@electron/fuses';
@@ -54,8 +54,8 @@ const config: ForgeConfig = {
   makers: [
     new MakerSquirrel({}), 
     new MakerZIP({}, ['darwin','linux']), 
-    new MakerRpm({}), 
-    new MakerDeb({}),
+    // new MakerRpm({}), 
+    // new MakerDeb({}),
   ],
   plugins: [
     new VitePlugin({
